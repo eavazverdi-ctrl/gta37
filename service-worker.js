9f896ae6984a0f734f4d04d5083015a6c905529e
Ehsan <e.avazverdi@gmail.com>
2025-09-21 23:27:39 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'fxlevels-v41';
+const CACHE = 'fxlevels-v45';
 const ASSETS = ['./','./index.html','./app.css','./app.js','./assets/bg.jpg','./assets/icons/icon-180.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
